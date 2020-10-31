1713a61eae5bd81a33c3b4fee3aec2d5d71e86a6
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-10-31 01:33:36 +0100

    update

---
+++ Data/CloudModelManager.cs
@@ -0,0 +1,85 @@
+﻿
+
+using Assignment2.Models.Unit;
+using DNP_Assignment2.Data;
+
+namespace Assignment2.Data
+{
+    public class CloudModelManager : IModelManager
+    {
+        public string AddUser(User newUser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Login(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UserList GetAllUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UpdatePassword(User oldUser, User newUser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddFamily(Family newFamily)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public FamilyList GetAllFamily()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UpdateFamily(Family oldFamily, Family newFamily)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveFamily(Family family)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddAdult(Adult newAdult)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AdultList GetAllAdult()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddChild(Child newChild)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ChildList GetAllChild()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UpdatePerson(Person newPerson)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePerson(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
